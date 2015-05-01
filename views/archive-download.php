29b7e2e5d38b51340eb462073758111a145dfdaa
Robin Cornett <hello@robincornett.com>
2015-05-01 16:13:32 -0400

    add post class to archive

--- views/archive-download.php
+++ views/archive-download.php
@@ -1,3 +1,11 @@
 <?php
 
+add_filter( 'post_class', 'edd_publications_archive_post_class', 10, 2 );
+function edd_publications_archive_post_class( $classes ) {
+	$classes[] = 'one-third';
+
+	return $classes;
+}
+
+
 genesis();
