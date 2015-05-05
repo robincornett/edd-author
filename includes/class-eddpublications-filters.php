9f231bb3fd6e1afd543c522b864e247d6cf1d407
Robin Cornett <hello@robincornett.com>
2015-05-05 09:25:49 -0400

    simplify variables

--- includes/class-eddpublications-filters.php
+++ includes/class-eddpublications-filters.php
@@ -5,20 +5,20 @@ class EDD_Publications_Filters {
 	public function __construct() {
 
 		// Labels
-		add_filter( 'edd_default_downloads_name', array( $this, 'edd_publications_default_names' ), 10, 2 );
-		add_filter( 'edd_download_tag_labels', array( $this, 'edd_publications_tag_labels' ), 10, 2 );
-		add_filter( 'edd_download_category_labels', array( $this, 'edd_publications_category_labels' ), 10, 2 );
+		add_filter( 'edd_default_downloads_name', array( $this, 'set_default_names' ), 10, 2 );
+		add_filter( 'edd_download_category_labels', array( $this, 'set_category_labels' ), 10, 2 );
+		add_filter( 'edd_download_tag_labels', array( $this, 'set_tag_labels' ), 10, 2 );
 
 		// Args
-		add_filter( 'edd_download_post_type_args', array( $this, 'edd_publications_post_type_args' ), 10, 2 );
-		add_filter( 'edd_download_category_args', array( $this, 'edd_publications_category_args' ), 10, 2 );
-		add_filter( 'edd_download_tag_args', array( $this, 'edd_publications_tag_args' ), 10, 2 );
+		add_filter( 'edd_download_post_type_args', array( $this, 'set_post_type_args' ), 10, 2 );
+		add_filter( 'edd_download_category_args', array( $this, 'set_category_args' ), 10, 2 );
+		add_filter( 'edd_download_tag_args', array( $this, 'set_tag_args' ), 10, 2 );
 
 		// Other
-		add_filter( 'edd_checkout_image_size', array( $this, 'edd_publications_checkout_image_size' ), 10, 2 );
+		add_filter( 'edd_checkout_image_size', array( $this, 'change_checkout_image' ), 10, 2 );
 	}
 
-	function edd_publications_default_names( $defaults ) {
+	function set_default_names( $defaults ) {
 		$defaults = array(
 			'singular' => __( 'Publication', 'edd-publications-rabia' ),
 			'plural'   => __( 'Publications', 'edd-publications-rabia' ),
@@ -27,64 +27,64 @@ class EDD_Publications_Filters {
 		return $defaults;
 	}
 
-	function edd_publications_category_labels( $category_labels ) {
-		$singular_label = 'Type';
-		$plural_label   = 'Types';
+	function set_category_labels( $category_labels ) {
+		$singular = 'Type';
+		$plural   = 'Types';
 
-		$category_labels['name']              = $plural_label;
-		$category_labels['singular_name']     = $singular_label;
-		$category_labels['search_items']      = sprintf( 'Search %s', $plural_label );
-		$category_labels['all_items']         = sprintf( 'All %s', $plural_label );
-		$category_labels['parent_item']       = sprintf( 'Parent %s', $singular_label );
-		$category_labels['parent_item_colon'] = sprintf( 'Parent %s:', $singular_label );
-		$category_labels['edit_item']         = sprintf( __( 'Edit %s', 'edd-publications-rabia' ), $singular_label );
-		$category_labels['update_item']       = sprintf( __( 'Update %s', 'edd-publications-rabia' ), $singular_label );
-		$category_labels['add_new_item']      = sprintf( __( 'Add New %s Type', 'edd-publications-rabia' ), $singular_label );
-		$category_labels['new_item_name']     = sprintf( 'New %s Name', $singular_label );
-		$category_labels['menu_name']         = $plural_label;
+		$category_labels['name']              = $plural;
+		$category_labels['singular_name']     = $singular;
+		$category_labels['search_items']      = sprintf( 'Search %s', $plural );
+		$category_labels['all_items']         = sprintf( 'All %s', $plural );
+		$category_labels['parent_item']       = sprintf( 'Parent %s', $singular );
+		$category_labels['parent_item_colon'] = sprintf( 'Parent %s:', $singular );
+		$category_labels['edit_item']         = sprintf( __( 'Edit %s', 'edd-publications-rabia' ), $singular );
+		$category_labels['update_item']       = sprintf( __( 'Update %s', 'edd-publications-rabia' ), $singular );
+		$category_labels['add_new_item']      = sprintf( __( 'Add New %s Type', 'edd-publications-rabia' ), $singular );
+		$category_labels['new_item_name']     = sprintf( 'New %s Name', $singular );
+		$category_labels['menu_name']         = $plural;
 
 		return $category_labels;
 	}
 
-	function edd_publications_tag_labels( $tag_labels ) {
-		$singular_label = 'Series';
-		$plural_label   = 'Series';
+	function set_tag_labels( $tag_labels ) {
+		$singular = 'Series';
+		$plural   = 'Series';
 
-		$tag_labels['name']              = $plural_label;
-		$tag_labels['singular_name']     = $singular_label;
-		$tag_labels['search_items']      = sprintf( 'Search %s', $plural_label );
-		$tag_labels['all_items']         = sprintf( 'All %s', $plural_label );
-		$tag_labels['parent_item']       = sprintf( 'Parent %s', $singular_label );
-		$tag_labels['parent_item_colon'] = sprintf( 'Parent %s:', $singular_label );
-		$tag_labels['edit_item']         = sprintf( __( 'Edit %s', 'edd-publications-rabia' ), $singular_label );
-		$tag_labels['update_item']       = sprintf( __( 'Update %s', 'edd-publications-rabia' ), $singular_label );
-		$tag_labels['add_new_item']      = sprintf( __( 'Add New %s Series', 'edd-publications-rabia' ), $singular_label );
-		$tag_labels['new_item_name']     = sprintf( 'New Series Name', $singular_label );
-		$tag_labels['menu_name']         = $plural_label;
+		$tag_labels['name']              = $plural;
+		$tag_labels['singular_name']     = $singular;
+		$tag_labels['search_items']      = sprintf( 'Search %s', $plural );
+		$tag_labels['all_items']         = sprintf( 'All %s', $plural );
+		$tag_labels['parent_item']       = sprintf( 'Parent %s', $singular );
+		$tag_labels['parent_item_colon'] = sprintf( 'Parent %s:', $singular );
+		$tag_labels['edit_item']         = sprintf( __( 'Edit %s', 'edd-publications-rabia' ), $singular );
+		$tag_labels['update_item']       = sprintf( __( 'Update %s', 'edd-publications-rabia' ), $singular );
+		$tag_labels['add_new_item']      = sprintf( __( 'Add New %s Series', 'edd-publications-rabia' ), $singular );
+		$tag_labels['new_item_name']     = sprintf( 'New Series Name', $singular );
+		$tag_labels['menu_name']         = $plural;
 
 		return $tag_labels;
 	}
 
-	function edd_publications_post_type_args( $download_args ) {
+	function set_post_type_args( $download_args ) {
 
 		$download_args['rewrite'] = array( 'slug' => 'works', 'with_front' => false );
 
 		return $download_args;
 	}
 
-	function edd_publications_category_args( $category_args ) {
+	function set_category_args( $category_args ) {
 		$category_args['rewrite'] = array( 'slug' => 'works', 'with_front' => false, 'hierarchical' => true );
 
 		return $category_args;
 	}
 
-	function edd_publications_tag_args( $tag_args ) {
+	function set_tag_args( $tag_args ) {
 		$tag_args['rewrite'] = array( 'slug' => 'series' );
 
 		return $tag_args;
 	}
 
-	function edd_publications_checkout_image_size( $size ) {
+	function change_checkout_image( $size ) {
 		$size = 'publication_checkout';
 
 		return $size;
